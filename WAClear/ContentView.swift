713ccc1631a39f6e1fde2a9c88f1374d742a013f
w0p03c5 <williamscott.paka@walmart.com>
2026-04-06 21:38:23 +0530

    Initial Commit

---
+++ WAClear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WAClear
+//
+//  Created by Willam Scott on 06/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WAClear/WAClearApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAClearApp.swift
+//  WAClear
+//
+//  Created by Willam Scott on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WAClearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
